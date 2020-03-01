4a421e7f4872f2b8ab9d3979506a8b6be4e3e76c
HIMANSHU SHARMA <ranubhardwaj89@gmail.com>
2020-03-01 15:52:15 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Dashboard(),
+      home: Home(),
     );
   }
 }
