61a3d0fbf33270b3aaead2475e37332a58b70e91
Himanshu Sharma <ranubhardwaj89@gmail.com>
2020-03-29 18:26:25 +0530

    splash screen updated

--- community_app_boilerplate/lib/app.dart
+++ community_app_boilerplate/lib/app.dart
@@ -1,8 +1,6 @@
 import 'dart:ui';
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:bordered_text/bordered_text.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -28,53 +26,24 @@ class _SplashScreen extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [0.3, 1],
-              colors: [Color(0xffff4000),Color(0xffffcc66),]
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              // stops: [0.02,0.6],
+              colors: [Color(0xffffffff),Color(0xffE46D39)]
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/gs_launcher.png',
-              width: 180.0,
-              height: 180.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top:150.0),
-              child: BorderedText(
-                strokeWidth: 0.0,
-                child: Text(
-                  '< CODE FOR FREEDOM >',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      decorationColor: Colors.amber[800],
-                      decoration: TextDecoration.none,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Fira Code',
-                      letterSpacing: 0.6,
-                      fontSize: 23.0
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top:120.0),
-              child: SpinKitPumpingHeart(
-                color: Colors.white,
-                size: 60.0,
-              ),
-            ),
-          ],
+        child: Center(
+          child: Image.asset(
+            'asset/images/gs_launcher.png',
+            width: 180.0,
+            height: 180.0,
+          ),
         ),
       ),
     );
