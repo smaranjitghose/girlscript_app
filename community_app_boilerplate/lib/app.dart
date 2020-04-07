8787b6e7c84e4a0fc1f449d18dd0e40055c4dd73
Anush Bhatia <anushbhatia1234@gmail.com>
2020-04-07 16:53:16 +0530

    New Mockups

--- community_app_boilerplate/lib/app.dart
+++ community_app_boilerplate/lib/app.dart
@@ -9,7 +9,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _SplashScreen extends State<MyApp> {
-
   startTime() async {
     var _duration = new Duration(seconds: 3);
     return new Timer(_duration, navigationPage);
@@ -33,22 +32,20 @@ class _SplashScreen extends State<MyApp> {
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
+              begin: Alignment.topRight, end: Alignment.bottomLeft,
               // stops: [0.02,0.6],
-              colors: [Color(0xffFE5858),Color(0xffEE9617)]
-          ),
+              colors: [Color(0xffFE5858), Color(0xffEE9617)]),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             SizedBox(
-              height: MediaQuery.of(context).size.height*0.1,
+              height: MediaQuery.of(context).size.height * 0.1,
             ),
             Container(
-              width: MediaQuery.of(context).size.width*0.39,
-              height: MediaQuery.of(context).size.height*0.2,
+              width: MediaQuery.of(context).size.width * 0.39,
+              height: MediaQuery.of(context).size.height * 0.2,
               decoration: BoxDecoration(
                 color: Colors.white,
                 // borderRadius: BorderRadius.all(Radius.circular(100)),
@@ -56,7 +53,7 @@ class _SplashScreen extends State<MyApp> {
                 boxShadow: [
                   BoxShadow(
                     color: Colors.black54,
-                    offset: Offset(0.0, 6.0), //(x,y)
+                    offset: Offset(0.0, 3.0), //(x,y)
                     blurRadius: 15.0,
                   ),
                 ],
@@ -69,19 +66,18 @@ class _SplashScreen extends State<MyApp> {
               ),
             ),
             SizedBox(
-              height: MediaQuery.of(context).size.height*0.026,
+              height: MediaQuery.of(context).size.height * 0.026,
             ),
             Text(
               'GIRLSCRIPT',
               style: TextStyle(
-                fontSize: 30.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.5
-              ),
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5),
             ),
             SizedBox(
-              height: MediaQuery.of(context).size.height*0.35,
+              height: MediaQuery.of(context).size.height * 0.35,
             ),
             SpinKitRing(
               color: Colors.white,
@@ -89,14 +85,11 @@ class _SplashScreen extends State<MyApp> {
               lineWidth: 6.0,
             ),
             SizedBox(
-              height: MediaQuery.of(context).size.height*0.03,
+              height: MediaQuery.of(context).size.height * 0.03,
             ),
             Text(
               'Let\'s Code For Freedom',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0
-              ),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
             )
           ],
         ),
