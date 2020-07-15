9cfc913992811f773da9c84ba5794f4f659c8dbd
Anush Bhatia <anushbhatia1234@gmail.com>
2020-07-16 02:28:12 +0530

    Bugs

--- community_app_boilerplate/lib/main.dart
+++ community_app_boilerplate/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:communityappboilerplate/services/auth.dart';
 import 'package:communityappboilerplate/services/user.dart';
 import 'package:communityappboilerplate/ui/dashboard.dart';
 import 'package:communityappboilerplate/ui/screens/home.dart';
-import 'package:communityappboilerplate/ui/screens/signUpScreen.dart';
+import 'package:communityappboilerplate/ui/screens/auth/signUpScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
@@ -10,34 +10,32 @@ import 'package:communityappboilerplate/ui/screens/auth/login.dart';
 
 void main() => runApp(MainApp());
 
-class MainApp extends StatelessWidget{
-  Widget build(BuildContext context){
+class MainApp extends StatelessWidget {
+  Widget build(BuildContext context) {
     return StreamProvider<User>.value(
       value: AuthService().user,
       child: MaterialApp(
-        home: _getScreenId(),
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Montserrat',
-        ),
-        routes: <String, WidgetBuilder>{
-          '/Login': (BuildContext context) => LoginScreen(),
-          '/SignUp': (BuildContext context) => SignUpScreen(),
-        }
-      ),
+          home: _getScreenId(),
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            fontFamily: 'Montserrat',
+          ),
+          routes: <String, WidgetBuilder>{
+            '/Login': (BuildContext context) => LoginScreen(),
+            '/SignUp': (BuildContext context) => SignUpScreen(),
+          }),
     );
   }
 
   Widget _getScreenId() {
     return StreamBuilder<FirebaseUser>(
-      stream: FirebaseAuth.instance.onAuthStateChanged,
-      builder: (BuildContext context, snapshot){
-        if(snapshot.hasData) {
-          return Home(snapshot.data.uid);
-        } else {
-          return SignUpScreen();
-        }
-      }
-    );
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return Home(snapshot.data.uid);
+          } else {
+            return SignUpScreen();
+          }
+        });
   }
 }
