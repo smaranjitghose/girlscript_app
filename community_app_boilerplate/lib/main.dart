e02b81d801d11b0b70416c5f25adf5ed6298352d
SirusCodes <darshandrander@gmail.com>
2020-10-03 17:13:51 +0530

    added intro screens

--- community_app_boilerplate/lib/main.dart
+++ community_app_boilerplate/lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:communityappboilerplate/services/auth.dart';
-import 'package:communityappboilerplate/services/user.dart';
-import 'package:communityappboilerplate/ui/dashboard.dart';
-import 'package:communityappboilerplate/ui/screens/auth/signUpScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:communityappboilerplate/ui/screens/auth/login.dart';
+import 'package:provider/provider.dart';
 
+import 'services/auth.dart';
+import 'services/user.dart';
+import 'ui/dashboard.dart';
+import 'ui/screens/auth/login.dart';
 import 'ui/screens/auth/signUpScreen.dart';
+import 'ui/screens/introScreen.dart';
 
 void main() => runApp(MainApp());
 
@@ -16,15 +16,15 @@ class MainApp extends StatelessWidget {
     return StreamProvider<User>.value(
       value: AuthService().user,
       child: MaterialApp(
-        home: _getScreenId(),
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Montserrat',
-        ),
-        routes: <String, WidgetBuilder>{
-          '/Login': (BuildContext context) => LoginScreen(),
-          '/SignUp': (BuildContext context) => SignUpScreen(),
-        }),
+          home: _getScreenId(),
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            fontFamily: 'Montserrat',
+          ),
+          routes: <String, WidgetBuilder>{
+            '/Login': (BuildContext context) => LoginScreen(),
+            '/SignUp': (BuildContext context) => SignUpScreen(),
+          }),
     );
   }
 
@@ -33,11 +33,11 @@ class MainApp extends StatelessWidget {
       stream: FirebaseAuth.instance.onAuthStateChanged,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return Dashboard(userId:snapshot.data.uid);
+          return Dashboard(userId: snapshot.data.uid);
         } else {
-          return LoginScreen();
+          return IntroScreen();
         }
-      }
+      },
     );
   }
 }
